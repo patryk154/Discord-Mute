b1101136b2bd1e12054729359f112911bf7223d0
patryk154 <83709913+patryk154@users.noreply.github.com>
2021-05-05 16:52:56 +0200

    Update mute.js

--- Discord-Mute/cmds/mute.js
+++ Discord-Mute/cmds/mute.js
@@ -1,63 +1,106 @@
-const fs = module.require("fs");
+cconst { MessageEmbed } = require("discord.js");
+const db = require('quick.db');
 
-module.exports.run = async (bot, message, args) => {
-
-    // Check perms, self, rank, etc
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have Permission to mute!");
-    let toMute = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!toMute) return message.channel.send("You did not specify a user mention or ID!");
-    if(toMute.id === message.author.id) return message.channel.send("You can not mute yourself!");
-    if(toMute.highestRole.position >= message.member.highestRole.position) return message.channel.send("You can not mute a member that is equal to or higher than yourself!");
-
-    // Check if the user has the mutedRole
-    let mutedRole = message.guild.roles.find(mR => mR.name === "Muted");
-
-    // If the mentioned user does not have the muted role execute the following
-    if(!mutedRole) {
+module.exports = {
+    config: {
+        name: "mute",
+        description: "Mutes a member in the discord!",
+        usage: "[name | nickname | mention | ID] <reason> (optional)",
+    },
+    run: async (bot, message, args) => {
         try {
-            // Create a role called "Muted"
-            mutedRole = await message.guild.createRole({
-                name: "Muted",
-                color: "#000000",
-                permissions: []
-            });
+            if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**Nie masz uprawnień, aby kogoś wyciszyć! - [MANAGE_GUILD]**");
 
-            // Prevent the user from sending messages or reacting to messages
-            message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(mutedRole, {
-                    SEND_MESSAGES: false,
-                    ADD_REACTIONS: false
-                })
-            });
+            if (!message.guild.me.hasPermission("MANAGE_GUILD")) return message.channel.send("**Nie mam uprawnień, aby kogoś wyciszyć! - [MANAGE_GUILD]**")
+            if (!args[0]) return message.channel.send("**Wprowadź użytkownika, którego chcesz wyciszyć!**");
 
-        } catch(e) {
-            // If err print
-            console.log(e.stack);
+            var mutee = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase());
+            if (!mutee) return message.channel.send("**Wprowadź prawidłowego użytkownika, którego chcesz wyciszyć!**");
+
+            if (mutee === message.member) return message.channel.send("**Nie możesz się wyciszyć!**")
+            if (mutee.roles.highest.comparePositionTo(message.guild.me.roles.highest) >= 0) return message.channel.send('**Nie można wyciszyć tego użytkownika!**')
+
+            let reason = args.slice(1).join(" ");
+            if (mutee.user.bot) return message.channel.send("**Nie można wyciszyć botów!**");
+            const userRoles = mutee.roles.cache
+                .filter(r => r.id !== message.guild.id)
+                .map(r => r.id)
+
+            let muterole;
+            let dbmute = await db.fetch(`muterole_${message.guild.id}`);
+            let muteerole = message.guild.roles.cache.find(r => r.name === "muted")
+
+            if (!message.guild.roles.cache.has(dbmute)) {
+                muterole = muteerole
+            } else {
+                muterole = message.guild.roles.cache.get(dbmute)
+            }
+
+            if (!muterole) {
+                try {
+                    muterole = await message.guild.roles.create({
+                        data: {
+                            name: "muted",
+                            color: "#514f48",
+                            permissions: []
+                        }
+                    })
+                    message.guild.channels.cache.forEach(async (channel) => {
+                        await channel.createOverwrite(muterole, {
+                            SEND_MESSAGES: false,
+                            ADD_REACTIONS: false,
+                            SPEAK: false,
+                            CONNECT: false,
+                        })
+                    })
+                } catch (e) {
+                    console.log(e);
+                }
+            };
+
+            if (mutee.roles.cache.has(muterole.id)) return message.channel.send("**Użytkownik jest już wyciszony!**")
+
+            db.set(`muteeid_${message.guild.id}_${mutee.id}`, userRoles)
+          try {
+            mutee.roles.set([muterole.id]).then(() => {
+                mutee.send(`**Witaj, zostałeś wyciszony ${message.guild.name} za - ${reason || "No Reason"}`).catch(() => null)
+            })
+            } catch {
+                 mutee.roles.set([muterole.id])                               
+            }
+                if (reason) {
+                const sembed = new MessageEmbed()
+                    .setColor("GREEN")
+                    .setAuthor(message.guild.name, message.guild.iconURL())
+                    .setDescription(`${mutee.user.username} został pomyślnie wyciszony za ${reason}`)
+                message.channel.send(sembed);
+                } else {
+                    const sembed2 = new MessageEmbed()
+                    .setColor("GREEN")
+                    .setDescription(`${mutee.user.username} został pomyślnie wyciszony`)
+                message.channel.send(sembed2);
+                }
+            
+            let channel = db.fetch(`modlog_${message.guild.id}`)
+            if (!channel) return;
+
+            let embed = new MessageEmbed()
+                .setColor('RED')
+                .setThumbnail(mutee.user.displayAvatarURL({ dynamic: true }))
+                .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
+                .addField("**Moderation**", "mute")
+                .addField("**Mutee**", mutee.user.username)
+                .addField("**Moderator**", message.author.username)
+                .addField("**Reason**", `${reason || "**No Reason**"}`)
+                .addField("**Date**", message.createdAt.toLocaleString())
+                .setFooter(message.member.displayName, message.author.displayAvatarURL())
+                .setTimestamp()
+
+            var sChannel = message.guild.channels.cache.get(channel)
+            if (!sChannel) return;
+            sChannel.send(embed)
+        } catch {
+            return;
         }
     }
-
-    // If the mentioned user already has the "mutedRole" then that can not be muted again
-    if(toMute.roles.has(mutedRole.id)) return message.channel.send("This user is already muted!");
-
-    // TODO: Check they they have entered a valid number or even entered one
-
-    // Check current time and add muted time to it, then convert to seconds from milliseconds
-    bot.muted[toMute.id] = {
-        guild: message.guild.id,
-        time: Date.now() + parseInt(args[1]) * 1000
-    }
-
-    // Add the mentioned user to the "mutedRole" and notify command sender
-    await toMute.addRole(mutedRole);
-    
-
-    fs.writeFile("./muted.json", JSON.stringify(bot.muted, null, 4), err => {
-        if(err) throw err;
-        message.channel.send(`I have muted ${toMute.user.tag}!`);
-    });
-
-}
-
-module.exports.help = {
-    name: "mute"
 }
